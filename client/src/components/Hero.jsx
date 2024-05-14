f10fe3bc5f8138032120d33fc9016d65a2c23cc0
Ankit Kumar <71378462+Ankit62042@users.noreply.github.com>
2024-05-14 09:01:19 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,13 +7,14 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-content">
         <h1>
-          Your Health, <br />
-          Our Responsibility
+          Empowering Health, Anywhere, Anytime, <br />
+          Bringing Doctors to Your Doorstep Digitally!
         </h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          tenetur doloremque molestias repellat minus asperiores in aperiam
-          dolor, quaerat praesentium.
+          Introducing our innovative app connecting doctors and patients seamlessly, 
+          regardless of location. Say goodbye to long waits and hello to convenience with appointment scheduling at your fingertips. 
+          Easily find the right doctor for your needs and book appointments effortlessly. 
+          Experience healthcare on your terms, simplified like never before!
         </p>
       </div>
       <div className="hero-img">
